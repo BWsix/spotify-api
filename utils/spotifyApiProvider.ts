cddb596e74d63e6cc984d344b45cc849a6ba693d
BWsix <itisbw62@gmail.com>
2021-10-23 21:40:50 +0800

    feat(SpotifyApi): added a global spotifyApiClient

---
+++ utils/spotifyApiProvider.ts
@@ -0,0 +1,32 @@
+import { assert } from "console";
+import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
+import SpotifyWebApi from "spotify-web-api-node";
+
+assert(
+  typeof process.env.SPOTIFY_CLIENT_ID === "string",
+  "a spotify client must be provided."
+);
+assert(
+  typeof process.env.SPOTIFY_CLIENT_SECRET === "string",
+  "a spotify client secret must be provided."
+);
+
+export const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+  redirectUri: process.env.SPOTIFY_CALLBACK_URL,
+});
+
+export const ensureHasToken = (apiRoute: NextApiHandler) => {
+  return async (req: NextApiRequest, res: NextApiResponse) => {
+    if (!spotifyApi.getAccessToken()) {
+      const {
+        body: { access_token },
+      } = await spotifyApi.clientCredentialsGrant();
+
+      spotifyApi.setAccessToken(access_token);
+    }
+
+    return await apiRoute(req, res);
+  };
+};
