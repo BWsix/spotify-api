bea433632117a58f13acd244634d102b4e468543
BWsix <itisbw62@gmail.com>
2021-10-23 23:42:09 +0800

    chore: added redirect

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://github.com/BWsix/spotify-api',
+        permanent: true,
+      },
+    ]
+  },
 }
