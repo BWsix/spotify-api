53dd79da0be69decae20dd7e66b599e65e8821fa
BWsix <itisbw62@gmail.com>
2021-10-24 16:46:06 +0800

    fix(user/playlists): changed to method `post` & added optinos field

--- pages/api/user/[id]/playlists.ts
+++ pages/api/user/[id]/playlists.ts
@@ -2,12 +2,13 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { spotifyApi } from "utils/spotifyApiProvider";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method !== "GET") return res.status(405).end();
+  if (req.method !== "POST") return res.status(405).end();
 
   const id = req.query.id as string;
+  const options = req.body.options;
 
   await spotifyApi.ensureHasToken();
-  spotifyApi.getUserPlaylists(id, {}, (error, result) => {
+  spotifyApi.getUserPlaylists(id, options, (error, result) => {
     if (error) {
       return res.status(500).json({ error: error.message });
     }
